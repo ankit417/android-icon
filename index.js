5f6d082cee122e0af5a918dcf695e16e57185564
Ankeet <acharya.ankit417@gmail.com>
2020-12-03 08:34:07 +0545

    handling error

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
 const iconGenerator = require("./script/generate");
 var arguments = process.argv;
-iconGenerator.generateIcon(arguments[2]);
+if (arguments[2]) {
+  iconGenerator.generateIcon(arguments[2]);
+} else {
+  console.log("Image name required");
+}
